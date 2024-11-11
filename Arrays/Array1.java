a40d43f261746842e84e56cad537984522f47002
Antonio <154091923+PIKKRR@users.noreply.github.com>
2024-11-11 23:20:17 +0100

    Update Array1.java

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -5,7 +5,7 @@ El programa deberá realizar lo siguiente:
 Inicializar un array de enteros con tamaño 10.
 Llenar el array con los números del 1 al 10 en orden ascendente.
 Imprimir los elementos del array en orden descendente.
- Autor: Jose P. Couso  "Blayneraptor" */ 
+Autor: Antonio Casamitjana 'PIKKRR' */ 
 
 
 public class Array1 {
