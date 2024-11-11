f43741affd6e940280f7866fd93f0b6583a17143
Antonio <154091923+PIKKRR@users.noreply.github.com>
2024-11-11 23:15:17 +0100

    Update DosNumeros.java

--- Condicionales/DosNumeros.java
+++ Condicionales/DosNumeros.java
@@ -6,7 +6,7 @@ Desarrolla un programa que solicite al usuario ingresar dos números.
 Si el primer número es mayor que el segundo, el programa multiplica el primero por dos
 y divide el segundo entre dos. Si el segundo número es mayor, entonces el programa
 divide el primero entre dos y multiplica el segundo por dos.
-Autor: Jose P. Couso "Blayneraptor"
+Autor: Antonio Casamitjana 'PIKKRR'
 */
 
 public class DosNumeros {
