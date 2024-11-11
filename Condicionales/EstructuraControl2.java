0c19a104a3bee301488e7f1f7cc4577d689c1fd5
Antonio <154091923+PIKKRR@users.noreply.github.com>
2024-11-11 23:14:41 +0100

    Update EstructuraControl2.java

--- Condicionales/EstructuraControl2.java
+++ Condicionales/EstructuraControl2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /*
 Desarrolla un programa que solicite al usuario ingresar un número entero.
 El programa determinará si el número ingresado es par o impar y mostrará un mensaje correspondiente.
-Autor: Jose P. Couso "Blayneraptor"
+Autor: Antonio Casamitjana 'PIKKRR'
 */
 
 public class EstructuraControl2 {
