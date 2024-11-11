83158b0289026c3fbb084bcbfcdebb15fea09abc
Antonio <154091923+PIKKRR@users.noreply.github.com>
2024-11-11 23:16:37 +0100

    Update IfParoImpar.java

--- Condicionales/IfParoImpar.java
+++ Condicionales/IfParoImpar.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /*
 Desarrolla un programa que solicite al usuario ingresar un número.
 Determina si el número ingresado es positivo o negativo, y muestra el resultado.
-Autor: Jose P. Couso "Blayneraptor"
+Autor: Antonio Casamitjana 'PIKKRR'
 */
 
 public class IfParoImpar {
